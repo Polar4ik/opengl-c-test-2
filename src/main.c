abf887d346038675ac28e2fb243794281cd37c2c
Polar4ik <atossnikitoss@gmail.com>
2024-09-26 23:11:57 +0400

    Add logs

--- src/main.c
+++ src/main.c
@@ -1,23 +1,27 @@
 #define GLEW_STATIC
 
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+#include<stdio.h>
 
 int main() {
-    // Инициализация GLFW
     if (!glfwInit()) {
+        printf("Failed to init GLFW");
         return -1;
     }
 
-    // Создание окна и контекста OpenGL
     GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Example", NULL, NULL);
     if (!window) {
+        printf("Failed to init window\n");
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
     
-    glewInit();
+    if (glewInit() != GLEW_OK) {
+        printf("GLEW initialization error\n");
+        return -1;
+    }
 
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
