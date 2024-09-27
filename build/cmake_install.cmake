f50b078ed73caef530b0b5c1ff72ca867c3b426d
Polar4ik <atossnikitoss@gmail.com>
2024-09-27 15:09:27 +0400

    Add triangles and shaders

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/All Projects/copengltest3
+# Install script for directory: D:/All Projects/c_opengl_test_3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -47,6 +47,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "D:/All Projects/copengltest3/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "D:/All Projects/c_opengl_test_3/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- src/main.c
+++ src/main.c
@@ -3,6 +3,53 @@
 #include<GL/glew.h>
 #include<GLFW/glfw3.h>
 #include<stdio.h>
+#include<stdlib.h>
+
+char* loadShaderSource(const char* filename) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Failed to open file: %s\n", filename);
+        return NULL;
+    }
+
+    fseek(file, 0L, SEEK_END);
+    long length = ftell(file);
+    rewind(file);
+
+    char* shaderSource = (char*)malloc(length + 1);
+    if (!shaderSource) {
+        printf("Failed to allocate memory for shader\n");
+        fclose(file);
+        return NULL;
+    }
+
+    fread(shaderSource, 1, length, file);
+    shaderSource[length] = '\0';
+
+    fclose(file);
+    return shaderSource;
+}
+
+void checkShaderCompilation(unsigned int shader) {
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        printf("ERROR::SHADER::COMPILATION_FAILED\n%s\n", infoLog);
+    }
+}
+
+void checkProgramLinking(unsigned int program) {
+    int success;
+    char infoLog[512];
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(program, 512, NULL, infoLog);
+        printf("ERROR::PROGRAM::LINKING_FAILED\n%s\n", infoLog);
+    }
+}
+
 
 int main() {
     if (!glfwInit()) {
@@ -23,14 +70,85 @@ int main() {
         return -1;
     }
 
+    unsigned int VAO, VBO, EBO;
+
+    float vecrtices[] = {
+         0.0f, 0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+    };
+
+    unsigned indices[] = {
+        0, 1, 2
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vecrtices), vecrtices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+
+    const char* vertex_shader_code = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main() {\n"
+        "   gl_Position = vec4(aPos, 1.0);\n"
+        "}\0";
+
+    const char* fragment_shader_code = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main() {\n"
+        "   FragColor = vec4(1.0, 1.0, 1.0, 1.0f);\n"
+        "}\0";
+
+    unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_code, NULL);
+    glCompileShader(vertex_shader);
+    checkShaderCompilation(vertex_shader);
+
+    unsigned int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_code, NULL);
+    glCompileShader(fragment_shader);
+    checkShaderCompilation(fragment_shader);
+
+    unsigned int shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+    checkProgramLinking(shader_program);
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(0.0f, 0.74f, 1.0f, 1.0f);
+
+        glUseProgram(shader_program);
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
 
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
+    glDeleteProgram(shader_program);
 
     glfwDestroyWindow(window);
     glfwTerminate();
